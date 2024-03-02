d9040e532cf7f7c9d18bd54c9dd7ec353858aca1
dropperDEV <jamescosta4747@Gmail.com>
2024-03-02 16:38:48 -0300

    feat: add Main

---
+++ src/ui/Main.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+const ContainerMain = styled.main`
+    background-color: green;
+    width: 900px;
+    height: 94vh;
+    border-radius: 10px;
+    border: 1.5px solid black;
+    margin-top: 5px
+`
+
+function Main() {
+  return (
+    <ContainerMain>
+        b
+    </ContainerMain>
+  )
+}
+
+export default Main
