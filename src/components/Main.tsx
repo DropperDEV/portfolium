e01218f3577b0ebd22ce246315466c4591e80a66
dropperDEV <jamescosta4747@Gmail.com>
2024-03-06 20:47:50 -0300

    feat: add implements css variables in <Main/>

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,33 +1,30 @@
 import styled from "styled-components";
-import {Link} from "react-router-dom";
-import {colors,device, shadows} from "../utils/StyleVars.ts";
+import { Link } from "react-router-dom";
+import { colors, device, shadows } from "../utils/StyleVars.ts";
 
 const ContainerMain = styled.main`
   width: 900px;
   height: 1400px;
   border-radius: 10px;
-  border: 1px solid hsl(0, 0%, 22%);
+  border: 1px solid ${colors.jet};
   margin-top: 20px;
   box-shadow: ${shadows.shadow5};
-  background-color: hsla(240, 2%, 12%, 0.418);
+  background-color: ${colors.eerieBlack2};
+
   @media ${device.lg} {
     width: 90%;
-
     align-items: baseline;
     justify-content: normal;
     position: relative;
     padding: 4px;
-
-  }
-  
-  @media ${device.md}{
-    width: 90%;
-
   }
 
-  @media ${device.sm}{
+  @media ${device.md} {
     width: 90%;
+  }
 
+  @media ${device.sm} {
+    width: 90%;
   }
 `;
 
@@ -42,31 +39,32 @@ const Navigation = styled.nav`
   border-radius: 12px 12px 0 0;
   box-shadow: ${shadows.shadow2};
   z-index: 5;
-
-`
+`;
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
-function Main({children}) {
-  return <ContainerMain>
-    <Navigation>
-      <ul>
-        <li>
-          <Link to={"/"}>Sobre Mim</Link>
-        </li>
-        <li>
-          <Link to={"/"}>Projetos</Link>
-        </li>
-        <li>
-          <Link to={"/"}>Certificados</Link>
-        </li>
-        <li>
-          <Link to={"/"}>Contato</Link>
-        </li>
-      </ul>
-    </Navigation>
-    {children}
-  </ContainerMain>;
+function Main({ children }) {
+  return (
+      <ContainerMain>
+        <Navigation>
+          <ul>
+            <li>
+              <Link to={"/"}>Sobre Mim</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Projetos</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Certificados</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Contato</Link>
+            </li>
+          </ul>
+        </Navigation>
+        {children}
+      </ContainerMain>
+  );
 }
 
 export default Main;
