539a4c6b50b66537e3b7ae2f434d23933d9c7434
dropperDEV <jamescosta4747@Gmail.com>
2024-03-06 21:51:55 -0300

    feat: add responsiviness in navigation

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,15 @@
 import styled from "styled-components";
-import {  NavLink } from "react-router-dom";
-import {colors, device, shadows, transitions} from "../utils/StyleVars.ts";
+import { NavLink } from "react-router-dom";
+import {
+  colors,
+  device,
+  shadows,
+  transitions,
+  typography,
+} from "../utils/StyleVars.ts";
 
 const ContainerMain = styled.main`
+  position: relative;
   width: 900px;
   height: 1400px;
   border-radius: 10px;
@@ -29,17 +36,17 @@ const ContainerMain = styled.main`
 `;
 
 const Navigation = styled.nav`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
+  position: absolute;
+  bottom: auto;
+  top: 0;
+  left: auto;
+  right: 0;
+  width: max-content;
+  border-radius: 0 0 0 20px;
+  padding: 0 10px;
   background: hsla(240, 1%, 17%, 0.75);
-  backdrop-filter: blur(10px);
-  border: ${colors.jet};
-  border-radius: 12px 12px 0 0;
-  box-shadow: ${shadows.shadow2};
   z-index: 5;
-  
+
   ul {
     display: flex;
     flex-wrap: wrap;
@@ -47,16 +54,18 @@ const Navigation = styled.nav`
     align-items: center;
     padding: 0 10px;
     list-style: none;
+    gap: 30px;
+    padding: 0 20px;
   }
-  
+
   a {
     color: ${colors.lightGray};
-    font-size: 15px; 
+    font-size: 15px;
     padding: 20px 7px;
     transition: color ${transitions.transition1};
     text-decoration: none;
   }
-  
+
   a:hover,
   a:focus {
     color: ${colors.lightGray70};
@@ -66,37 +75,57 @@ const Navigation = styled.nav`
     color: ${colors.orange};
   }
 
+  @media ${device.md} {
+    position: fixed;
+    bottom: 0;
+    top: auto;
+    left: 0;
+    width: 100%;
+    background: hsla(240, 1%, 17%, 0.75);
+    backdrop-filter: blur(10px);
+    border: ${colors.jet};
+    border-radius: 12px 12px 0 0;
+    box-shadow: ${shadows.shadow2};
+    z-index: 5;
+
+    ul{
+      gap: 0;
+    }
+
+    a {
+      font-weight: ${typography.fw500};
+    }
+  }
   @media ${device.sm} {
-    a{
-      font-size: 11px; 
+    a {
+      font-size: 11px;
     }
   }
 `;
-
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 function Main({ children }) {
   return (
-      <ContainerMain>
-        <Navigation>
-          <ul>
-            <li>
-              <NavLink to={"/"}>Sobre Mim</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/projects"}>Projetos</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/certificates"}>Certificados</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/contact"}>Contato</NavLink>
-            </li>
-          </ul>
-        </Navigation>
-        {children}
-      </ContainerMain>
+    <ContainerMain>
+      <Navigation>
+        <ul>
+          <li>
+            <NavLink to={"/"}>Sobre Mim</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/projects"}>Projetos</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/certificates"}>Certificados</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/contact"}>Contato</NavLink>
+          </li>
+        </ul>
+      </Navigation>
+      {children}
+    </ContainerMain>
   );
 }
 
