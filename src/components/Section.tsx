33e7d41dcd2c93fc31465463b7dd1b5ca1123f1c
dropperDEV <jamescosta4747@Gmail.com>
2024-03-09 20:27:05 -0300

    feat: add animation in title section

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { typography } from "../utils/StyleVars";
+import { colors, transitions, typography } from "../utils/StyleVars";
 import styled from "styled-components";
 
-interface ISection{
-    title: string;
-    children: any;
+interface ISection {
+  title: string;
+  children: any;
 }
 
 const StyledSection = styled.section`
@@ -19,9 +19,28 @@ const StyledSection = styled.section`
 const StyledSectionTitle = styled.p`
   font-size: ${typography.fs1};
   font-weight: ${typography.fw600};
+  padding-bottom: 10px;
+
+  position: relative;
+
+  &:after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    min-width: 10px;
+    height: 4px;
+    background: ${colors.orange};
+    border-radius: 3px;
+    transition: ${transitions.transition1};
+  }
+
+  &:hover:after {
+    min-width: 100%; /* Change to min-width for consistency */
+  }
 `;
 
-export default function Section({title,children}: ISection) {
+export default function Section({ title, children }: ISection) {
   return (
     <StyledSection>
       <StyledSectionTitle>{title}</StyledSectionTitle>
