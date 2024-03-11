65b85e389ca7ec448f6490ca16fd4781f0411a66
dropperDEV <jamescosta4747@Gmail.com>
2024-03-10 22:40:35 -0300

    feat: add Skill Section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,6 +7,88 @@ import { FaMobileScreenButton } from "react-icons/fa6";
 import { BsDatabaseFillGear } from "react-icons/bs";
 import { TbMathFunction } from "react-icons/tb";
 import SectionItem from "../ui/SectionItem";
+import styled from "styled-components";
+import { colors, gradients, typography } from "../utils/StyleVars";
+
+const StyledBigContainer = styled.div`
+  background: ${gradients.bgGradientJet};
+  padding: 40px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 20px;
+  grid-row-gap: 0px;
+  border-radius: 15px;
+`;
+
+const StyledHalfContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+const StyledSkillList = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(5, 1fr);
+  grid-column-gap: px;
+  grid-row-gap: 20px;
+  padding: 20px;
+`;
+
+const StyledSkillListTitle = styled.p`
+  font-size: 20px;
+  text-transform: capitalize;
+  font-weight: ${typography.fw600};
+  text-align: center;
+`;
+
+const StyledSkillItem = styled.li`
+  display: flex;
+  gap: 5px;
+  flex-direction: column;
+`;
+
+const StyledSkillWrapperTitle = styled.div`
+  display: flex;
+  gap: 4px;
+
+  p:first-child {
+    font-weight: ${typography.fw600};
+    font-size: ${typography.fs6};
+  }
+  p {
+    color: ${colors.lightGray};
+    font-size: ${typography.fs7};
+    font-weight: ${typography.fw300};
+  }
+`;
+
+const StyledSkillProgress = styled.progress`
+  -webkit-appearance: none;
+  appearance: none;
+  width: 100%;
+  height: 7px;
+  background: ${colors.jet}; /* Cor de fundo da barra de progresso */
+  border: none;
+  border-radius: 10px;
+  color: ${colors.orange}; /* Cor de fundo da barra de progresso não preenchida */
+
+  &::-webkit-progress-bar {
+    color: ${colors.orange}; /* Cor de fundo da barra de progresso não preenchida */
+    border-radius: 10px;
+  }
+
+  &::-webkit-progress-value {
+    color: ${colors.orange}; /* Cor da parte preenchida da barra de progresso */
+    border-radius: 10px;
+  }
+
+  &:-moz-progress-bar {
+    color: ${colors.orange}; /* Cor da parte preenchida da barra de progresso */
+    border-radius: 10px;
+  }
+`;
 
 function About() {
   return (
@@ -62,6 +144,90 @@ function About() {
             }
           />
         </TwoColumsSection>
+      </Section>
+      <Section title="Linguagems">
+        <StyledBigContainer>
+          <StyledHalfContainer>
+            <StyledSkillListTitle>linguagems de código</StyledSkillListTitle>
+            <StyledSkillList>
+              <StyledSkillItem>
+                <StyledSkillWrapperTitle>
+                  <p>Javascript</p>
+                  <p>90%</p>
+                </StyledSkillWrapperTitle>
+                <StyledSkillProgress max={100} value={80}></StyledSkillProgress>
+              </StyledSkillItem>
+              <StyledSkillItem>
+                <StyledSkillWrapperTitle>
+                  <p>Javascript</p>
+                  <p>90%</p>
+                </StyledSkillWrapperTitle>
+                <StyledSkillProgress max={100} value={80}></StyledSkillProgress>
+              </StyledSkillItem>
+              <StyledSkillItem>
+                <StyledSkillWrapperTitle>
+                  <p>Javascript</p>
+                  <p>90%</p>
+                </StyledSkillWrapperTitle>
+                <StyledSkillProgress max={100} value={80}></StyledSkillProgress>
+              </StyledSkillItem>
+              <StyledSkillItem>
+                <StyledSkillWrapperTitle>
+                  <p>Javascript</p>
+                  <p>90%</p>
+                </StyledSkillWrapperTitle>
+                <StyledSkillProgress max={100} value={80}></StyledSkillProgress>
+              </StyledSkillItem>
+              <StyledSkillItem>
+                <StyledSkillWrapperTitle>
+                  <p>Javascript</p>
+                  <p>90%</p>
+                </StyledSkillWrapperTitle>
+                <StyledSkillProgress max={100} value={80}></StyledSkillProgress>
+              </StyledSkillItem>
+            </StyledSkillList>
+          </StyledHalfContainer>
+          <StyledHalfContainer>
+            <StyledSkillListTitle>linguagems de código</StyledSkillListTitle>
+            <StyledSkillList>
+              <StyledSkillItem>
+                <StyledSkillWrapperTitle>
+                  <p>Javascript</p>
+                  <p>90%</p>
+                </StyledSkillWrapperTitle>
+                <StyledSkillProgress max={100} value={80}></StyledSkillProgress>
+              </StyledSkillItem>
+              <StyledSkillItem>
+                <StyledSkillWrapperTitle>
+                  <p>Javascript</p>
+                  <p>90%</p>
+                </StyledSkillWrapperTitle>
+                <StyledSkillProgress max={100} value={80}></StyledSkillProgress>
+              </StyledSkillItem>
+              <StyledSkillItem>
+                <StyledSkillWrapperTitle>
+                  <p>Javascript</p>
+                  <p>90%</p>
+                </StyledSkillWrapperTitle>
+                <StyledSkillProgress max={100} value={80}></StyledSkillProgress>
+              </StyledSkillItem>
+              <StyledSkillItem>
+                <StyledSkillWrapperTitle>
+                  <p>Javascript</p>
+                  <p>90%</p>
+                </StyledSkillWrapperTitle>
+                <StyledSkillProgress max={100} value={80}></StyledSkillProgress>
+              </StyledSkillItem>
+              <StyledSkillItem>
+                <StyledSkillWrapperTitle>
+                  <p>Javascript</p>
+                  <p>90%</p>
+                </StyledSkillWrapperTitle>
+                <StyledSkillProgress max={100} value={80}></StyledSkillProgress>
+              </StyledSkillItem>
+            </StyledSkillList>
+          </StyledHalfContainer>
+        </StyledBigContainer>
       </Section>
     </>
   );
