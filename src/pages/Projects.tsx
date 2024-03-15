4ae73b347ffe8be3f0b3c510f72d03e17beb729c
dropperDEV <jamescosta4747@Gmail.com>
2024-03-15 19:07:34 -0300

    feat: add nav in SectionProject

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,41 @@
+import styled from "styled-components";
+import TitleMain from "../ui/TitleMain";
+import { colors, typography } from "../utils/StyleVars";
+
+const StyledSectionProject = styled.section`
+  nav {
+    margin-top: 30px;
+  }
+  nav ul {
+    display: flex;
+    gap: 30px;
+  }
+  nav ul li {
+    color: ${colors.lightGray70};
+    font-size: ${typography.fs5};
+  }
+  nav ul li:hover {
+    color: ${colors.lightGray};
+    cursor: pointer;
+  }
+`;
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <>
+      <TitleMain text="Meus projetos" />
+      <StyledSectionProject>
+        <nav>
+          <ul>
+            <li>Todos</li>
+            <li>Websites</li>
+            <li>Jogos</li>
+            <li>Aplicações</li>
+          </ul>
+        </nav>
+      </StyledSectionProject>
+    </>
+  );
 }
 
-export default Projects
+export default Projects;
