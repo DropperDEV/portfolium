c6f1d2ba7eb7a20c6c0375fc36b6282e32b3ca01
dropperDEV <jamescosta4747@Gmail.com>
2024-03-24 10:47:28 -0300

    feat: add CertificatesData.ts

---
+++ src/service/certificatesData.ts
@@ -0,0 +1,15 @@
+interface ICertificates{
+    title: string;
+    imgSrc: string;
+    institution: string;
+}
+
+const Certificates: ICertificates[] = [
+  {
+    title: "Formação React Developer",
+    imgSrc: "https://hermes.dio.me/certificates/cover/ITMC2TZB.jpg",
+    institution: "DIO",
+  },
+];
+
+export default Certificates;
